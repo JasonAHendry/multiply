6cdcf0e0229f4456d3c9356e6df1c3e336113b70
JasonAHendry <jason.ahendry@gmail.com>
2022-04-26 11:53:54 +0200

    Add download --all command.

--- src/multiply/download/commands.py
+++ src/multiply/download/commands.py
@@ -1,12 +1,13 @@
+from turtle import down
 import click
 from multiply.download.collection import genome_collection
 from multiply.download.gff import gff_standardisation_functions
 from multiply.download.downloaders import GenomeDownloader
 
 
-# @click.option("--all")
 @click.command(short_help="Download genome information.")
 @click.option("--available", is_flag=True, help="List genomes available for download.")
+@click.option("--all", is_flag=True, help="Download all genomes available.")
 @click.option(
     "-g",
     "--genome_name",
@@ -14,7 +15,7 @@ from multiply.download.downloaders import GenomeDownloader
     default=None,
     help="Genome to download; must exist in collection.",
 )
-def download(available, genome_name):
+def download(available, all, genome_name):
     """
     Utilities to download genome information for MULTIPLY
 
@@ -23,8 +24,8 @@ def download(available, genome_name):
     must be prepared separately.
 
     """
-    if not available and genome_name is None:
-        print("Must specify options. Type 'multiply download --help' for details.")
+    # if (not available) and (not all) and genome_name is None:
+    #     print("Must specify options. Type 'multiply download --help' for details.")
 
     # Print available
     if available:
@@ -37,15 +38,34 @@ def download(available, genome_name):
         print(f"{'Name':25} {'Source':15}")
         for _, genome in genome_collection.items():
             print(f"{genome.name:25} {genome.source:15}")
-
-    # Prepare downloader
-    downloader = GenomeDownloader()
-
-    # Download
-    if genome_name is not None:
+    elif all:
+        downloader = GenomeDownloader()
+        for genome_name, genome in genome_collection.items():
+            downloader.set_genome(genome)
+            downloader.download_fasta()
+            downloader.download_gff()
+            downloader.standardise_gff(gff_standardisation_functions[genome.source])
+            downloader.close_logging()
+    elif genome_name is not None:
+        downloader = GenomeDownloader()
         genome = genome_collection[genome_name]
         downloader.set_genome(genome)
         downloader.download_fasta()
         downloader.download_gff()
         downloader.standardise_gff(gff_standardisation_functions[genome.source])
         downloader.close_logging()
+    else:
+        print("Must specify options. Type 'multiply download --help' for details.")
+
+
+    # # Prepare downloader
+    # downloader = GenomeDownloader()
+
+    # # Download
+    # if genome_name is not None:
+    #     genome = genome_collection[genome_name]
+    #     downloader.set_genome(genome)
+    #     downloader.download_fasta()
+    #     downloader.download_gff()
+    #     downloader.standardise_gff(gff_standardisation_functions[genome.source])
+    #     downloader.close_logging()
