92a600d8f3a37017cf1313802d7f7c651b5e0b3b
JasonAHendry <jason.ahendry@gmail.com>
2022-04-26 21:13:30 +0200

    Moved genome collection into class.

--- src/multiply/download/collection.py
+++ src/multiply/download/collection.py
@@ -1,39 +1,98 @@
+import os
 import configparser
 from multiply.download.genomes import PlasmoDBFactory, EnsemblGenomesFactory
 
-INI_PATH = "genomes/collection.ini"  # need to check this exists
-factories = [PlasmoDBFactory, EnsemblGenomesFactory]
+
+# ================================================================================
+# Define a collection of Genome objects
+#
+# ================================================================================
 
 
-def gen_genome_collection(ini_path, factories):
-    """
-    Generate the a genome collection, based on
-    the current genome collectionn `ini_path` and
-    genome `factories`
+class GenomeCollection(dict):
+    def __init__(self, ini_path, factories):
+        """
+        Hold a collection of Genome objects as a dictionary,
+        defined from an initiation file located at `ini_path`
 
-    """
-    # Load genome collection from source
-    config = configparser.ConfigParser()
-    config.read(ini_path)
+        params
+            ini_path: str
+                Path to .ini file containing information
+                about defined genomes.
+            factories: list[GenomeFactory]
+                List of GenomeFactory instances, used to
+                create individual genomes based on
+                their source.
+        
+        """
 
-    # Create collection
-    genome_collection = {}
-    for section in config.sections():
+        # Ensure points to valid file, then set
+        if not os.path.isfile(ini_path):
+            raise FileNotFoundError(f"No genome collection found at {ini_path}. Check file path is correct.")
+        self._ini_path = ini_path
 
-        # Extract parameters
-        kwargs = dict(config.items(section))
-        source = kwargs.pop("source")
+        # Read config object
+        self._config = configparser.ConfigParser()
+        self._config.read(ini_path)
 
-        # Get suitable factory
-        (factory,) = [factory() for factory in factories if factory.source == source]
+        # Set the factories used to build Genome objects
+        self._factories = factories
 
-        # Create genome
-        genome = factory.create_genome(name=section, **kwargs)
+    def populate(self):
+        """
+        Populate the genome collection
 
-        # Store
-        genome_collection[genome.name] = genome
+        """
 
-    return genome_collection
+        for section in self._config.sections():
+
+            # Extract parameters
+            kwargs = dict(self._config.items(section))
+            source = kwargs.pop("source")
+
+            # Get suitable factory
+            (factory,) = [factory() for factory in self._factories if factory.source == source]
+
+            # Create genome
+            genome = factory.create_genome(name=section, **kwargs)
+
+            # Store
+            self[genome.name] = genome
+
+    def is_downloaded(self, genome_name):
+        """
+        Check if a given genome is downloaded
+        
+        """
+
+        has_fasta = os.path.isfile(self[genome_name].fasta_path)
+        has_gff = os.path.isfile(self[genome_name].gff_path)
+
+        return has_fasta and has_gff
+
+    def display(self):
+        """
+        Display the genome collection
+        
+        """
+
+        print("SUMMARY")
+        print(f"  Collection located at: {self._ini_path}")
+        print(f"  No. genomes in collection: {len(self)}")
+        print("")
+        print("BREAKDOWN")
+        print(f"  {'Name':25} {'Source':25} {'Downloaded [both FASTA and GFF]':10}")
+        for genome_name, genome in self.items():
+            print(f"  {genome.name:25} {genome.source:25} {self.is_downloaded(genome_name)!s:10}")
 
 
-genome_collection = gen_genome_collection(ini_path=INI_PATH, factories=factories)
+# ================================================================================
+# Initialise the genome collection
+# - Not sure I like this here
+# ================================================================================
+
+
+INI_PATH = "genomes/collection.ini"
+FACTORIES = [PlasmoDBFactory, EnsemblGenomesFactory]
+genome_collection = GenomeCollection(INI_PATH, FACTORIES)
+genome_collection.populate()
