49c8f1be989156c083489b767918b52a4e66a9f0
JasonAHendry <jason.ahendry@gmail.com>
2022-06-12 15:38:31 +0200

    Adding alignment to CLI.

--- src/multiply/cli.py
+++ src/multiply/cli.py
@@ -4,6 +4,7 @@ import logging
 from multiply.download.commands import download
 from multiply.generate.commands import generate
 from multiply.view.commands import view
+from multiply.align.commands import align
 
 
 # ================================================================
@@ -47,6 +48,7 @@ def cli():
 cli.add_command(download)
 cli.add_command(generate)
 cli.add_command(view)
+cli.add_command(align)
 
 if __name__ == "__main__":
     cli()
