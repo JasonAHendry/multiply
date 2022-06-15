a0c04c500488b44fa7c406195bb5928260f474b3
JasonAHendry <jason.ahendry@gmail.com>
2022-06-15 12:07:23 +0200

    Return predicted primer binding sites from BLAST results.

--- src/multiply/blast/annotator.py
+++ src/multiply/blast/annotator.py
@@ -5,7 +5,12 @@ from collections import namedtuple
 class BlastResultsAnnotator:
     def __init__(self, blast_df):
         """
-        Annotate tabular results produced by running BLAST
+        Annotate tabular results produced by running BLAST with
+        `-outfmt 6`. 
+
+        params
+            blast_df: DataFrame (n_hits, n_columns)
+                Each row contains a BLAST hit.
 
         """
         self.blast_df = blast_df
@@ -13,14 +18,15 @@ class BlastResultsAnnotator:
     def build_annotation_dict(self, length_threshold=12, evalue_threshold=4):
         """
         Build a dictionary specifying conditions for annotation
-        
+
         """
         self.annotations = {
             "from_3prime": lambda row: row["qend"] == row["length"],
             "length_pass_3prime": lambda row: row["length"] >= length_threshold
             and row["pident"] == 100
             and row["from_3prime"],
-            "evalue_pass_3prime": lambda row: row["evalue"] < 4 and row["from_3prime"],
+            "evalue_pass_3prime": lambda row: row["evalue"] < evalue_threshold
+            and row["from_3prime"],
             "predicted_bound": lambda row: row["length_pass_3prime"]
             or row["evalue_pass_3prime"],
         }
@@ -28,20 +34,38 @@ class BlastResultsAnnotator:
     def add_annotations(self):
         """
         Add annotations to `blast_df`
-        
+
         """
         for annot_name, annot_func in self.annotations.items():
             self.blast_df.insert(
-                self.blast_df.shape[1], 
-                annot_name, 
-                self.blast_df.apply(func=annot_func, axis=1)
+                self.blast_df.shape[1],
+                annot_name,
+                self.blast_df.apply(func=annot_func, axis=1),
             )
+
+    def get_predicted_bound(self):
+        """
+        Get all BLAST matches where we predict that the query
+        sequence will bind
+
+        """
+        if not "predicted_bound" in self.blast_df:
+            raise ValueError(
+                "`blast_df` must contain a column `predicted_bound`. Try running `.add_annotations()` first."
+            )
+
+        return self.blast_df.query("predicted_bound")
 
     def summarise_by_primer(self, output_path=None):
         """
         Summarise the BLAST results on a per-primer
         basis
-        
+
+        In essence, convert from a dataframe where each row
+        is a BLAST hit, to a dataframe where each row
+        is a primer, and columns give the total number of hits
+        for that primer.
+
         """
 
         # Define structure to hold per-primer summaries
@@ -64,9 +88,8 @@ class BlastResultsAnnotator:
         ]
 
         # Convert to data frame
-        self.blast_primer_df = (
-            pd.DataFrame(primer_blast_records)
-            .sort_values("predicted_bound", ascending=False)
+        self.blast_primer_df = pd.DataFrame(primer_blast_records).sort_values(
+            "predicted_bound", ascending=False
         )
 
         # Optionally write
