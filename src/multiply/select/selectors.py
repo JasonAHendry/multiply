7ae0a9bc5b2be530a7aa48ed5aa6e6c4e9255254
JasonAHendry <jason.ahendry@gmail.com>
2022-06-18 23:37:26 +0200

    Sketch of algorithms for selecting optimal multiplex.

---
+++ src/multiply/select/selectors.py
@@ -0,0 +1,77 @@
+import random
+from abc import ABC, abstractmethod
+
+class MultiplexSelector(ABC):
+    def __init__(self, primer_df, cost_function):
+        self.primer_df = primer_df
+        self.cost_function = cost_function
+        
+    @abstractmethod
+    def run(self):
+        """
+        Run the selection method
+        
+        """
+        pass
+
+
+class GreedySearch(MultiplexSelector):
+    """
+    Try to find the optimal multiplex using a greedy search algorithm
+    
+    """
+    
+    N = 50
+    
+    def run(self):
+        """
+        This runs but it seems to be insanely slow, even for an 8plex
+        
+        """
+        
+        # Get target pairs
+        target_pairs = {
+            target_id: target_df["pair_name"].tolist()
+            for target_id, target_df in self.primer_df.groupby("target_id")
+        }
+        
+        # IDs
+        target_ids = list(target_pairs)
+        
+        # Iterate
+        multiplexes = []
+        for ix in range(self.N):
+            
+            # Prepare empty new multiplex
+            multiplex = []
+            
+            # Shuffle the target IDs in place
+            random.shuffle(target_ids)
+            
+            # Compute scores of each possible pair
+            for target_id in target_ids:
+                costs = [
+                    self.cost_function.calc_cost(multiplex + [primer_pair])
+                    for primer_pair in target_pairs[target_id]
+                ]
+            
+                # Add max scoring from this step
+                idxmax = costs.index(min(costs))
+                multiplex.append(target_pairs[target_id][idxmax])
+            
+            # Add to list of all multiplexes
+            multiplexes.append((min(costs), multiplex))
+            
+            if ix % 10 == 0:
+                print(ix)
+            
+        return multiplexes
+
+class BruteForce(MultiplexSelector):
+    pass
+
+class SelectRandom(MultiplexSelector):
+    pass
+
+
+
