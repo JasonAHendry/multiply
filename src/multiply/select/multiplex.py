fe646b47bcdccd31d3e74500f05816174465d94e
JasonAHendry <jason.ahendry@gmail.com>
2022-06-20 13:02:39 +0200

    Encapulation of multiplex information.

---
+++ src/multiply/select/multiplex.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from itertools import product
+from typing import Tuple
+
+
+@dataclass(unsafe_hash=True, order=True)
+class Multiplex:
+    """
+    Encapsulate information about a multiplex in a manner
+    that allows for sorting (primarily by cost) and hashing,
+    such that a unique set of multiplexes can be found.
+
+    To make hashable, the `.primer_pairs` attribute is
+    co-erced into a tuple during `__post_init__`.
+
+    """
+
+    cost: float
+    primer_pairs: Tuple[str]
+    method: str = ""
+
+    def __post_init__(self):
+        self.primer_pairs.sort()
+        self.primer_pairs = tuple(self.primer_pairs)
+
+    def get_primer_names(self):
+        directions = ["F", "R"]
+        return [f"{p}_{d}" for p, d in product(self.primer_pairs, directions)]
