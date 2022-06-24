a3e3db9a46b2f225951dac588ef3eda5ebd04c75
JasonAHendry <jason.ahendry@gmail.com>
2022-06-24 14:59:00 +0200

    Class for exploring output multiplexes.

--- src/multiply/select/commands.py
+++ src/multiply/select/commands.py
@@ -5,7 +5,7 @@ from multiply.util.printing import print_header, print_footer
 from .cost.factories import IndividualCostFactory, PairwiseCostFactory
 from .cost.functions import LinearCost
 from .selectors import GreedySearch
-
+from .explore import MultiplexExplorer
 
 # PARAMETERS
 INDV_INI_PATH = "settings/select/individual_costs.ini"
@@ -68,7 +68,7 @@ def main(result_dir):
         .normalise_costs()
         for indv_cost in indv_factory.get_individual_costs()
     ]
-    print(f"  Individual costs: {' ,'.join([i.cost_name for i in indv_costs])}")
+    print(f"  Individual costs: {', '.join([i.cost_name for i in indv_costs])}")
 
     # CREATE PAIRWISE COSTS
     pairwise_factory = PairwiseCostFactory(PAIR_INI_PATH, result_dir)
@@ -78,7 +78,7 @@ def main(result_dir):
         .normalise_costs()
         for pair_cost in pairwise_factory.get_pairwise_costs()
     ]
-    print(f"  Pairwise costs: {' ,'.join([i.cost_name for i in indv_costs])}")
+    print(f"  Pairwise costs: {', '.join([i.cost_name for i in indv_costs])}")
 
     # SET COST FUNCTION
     print("Building cost function...")
@@ -87,43 +87,27 @@ def main(result_dir):
     print("Done.\n")
 
     # SET SELECTION ALGORITHM
-    print(f"Seaching for top {N_SELECT} optimal multiplexes...")
-    print(f"  Search algorithm: Greedy")
+    print(f"Seaching for optimal multiplexes...")
+    print(f" Algorithm: Greedy")
     selector = GreedySearch(primer_df, cost_function)
     multiplexes = selector.run()
 
-    # FORMAT OUTPUTS
-    print("Writing output...")
-    # Reduce to unique and sort
-    final_multiplexes = sorted(set(multiplexes))
+    # EXPLORE OUTPUTS
+    print("Exploring resulting multiplexes...")
+    explorer = MultiplexExplorer(primer_df, multiplexes)
+    explorer.set_top_multiplexes(top_N=N_SELECT)
+    print(f"  Total multiplexes generated: {len(explorer.multiplexes)}")
+    print(f"  No. unique: {len(explorer.uniq_multiplexes)}")
+    print(f"  Top {len(explorer.top_multiplexes)} retained.")
 
-    # Create a summary data frame
-    dfs = [
-        primer_df.loc[final_multiplexes[ix].get_primer_names()]
-        for ix in range(N_SELECT)
-    ]
-    top_multiplexes = (
-        pd.concat(dfs)
-        .reset_index(drop=True)
-        .fillna(False)
-        .drop_duplicates("primer_name")
-        .sort_values("primer_name")
-    )
-    for ix in range(N_SELECT):
-        top_multiplexes.insert(
-            ix+4, 
-            f"in_multiplex{ix:02d}", 
-            [p in final_multiplexes[ix].primer_pairs 
-            for p in top_multiplexes["pair_name"]])
-    overview_csv = f"{output_dir}/table.multiplexes_overview.csv"
-    top_multiplexes.to_csv(overview_csv)
-    print(f"  to: {overview_csv}")
+    # WRITE OUTPUTS
+    print("Writing outputs...")
+    info_path = f"{output_dir}/table.multiplexes.information.csv"
+    order_path = f"{output_dir}/table.multiplexes.order.csv"
+    explorer.get_union_dataframe(f"{output_dir}/table.multiplexes_overview.csv")
+    explorer.get_order_dataframe(f"{output_dir}/table.multiplexes_order.csv")
+    print(f"  Information about primers in multiplexes: {info_path}")
+    print(f"  Primer ordering table: {order_path}")
     print("Done.\n")
 
     print_footer(t0)
-
-    # Sketch of OO for future
-    # output_formatter = MultiplexFormatter(primer_df, multiplexes)
-    # output_formatter.set_top_multiplexes(N=3)
-    # output_formatter.write_joint_dataframe(f"{output_dir}/select/table.multiplexes_overview.csv")
-    # output_formatter.write_order_dataframe(f"{output_dir}/select/table.multiplexes_order.csv")
---
+++ src/multiply/select/explore.py
@@ -0,0 +1,106 @@
+import pandas as pd
+
+
+class MultiplexExplorer:
+    def __init__(self, primer_df, multiplexes):
+        """
+        Explore `multiplexes` derived from a given `primer_df`
+
+        params
+            primer_df: pandas DataFrame
+                Each row is an individual primer.
+            multiplexes: List[Multiplex]
+                List of multiplex objects
+
+        """
+
+        # Set at instantiation
+        self.primer_df = primer_df
+        self.multiplexes = multiplexes
+
+        # Compute
+        self._get_unique_multiplexes_and_sort()
+
+    def _get_unique_multiplexes_and_sort(self):
+        """
+        Reduce list of multiplexes to a unique set,
+        and sort
+
+        """
+
+        self.uniq_multiplexes = sorted(set(self.multiplexes))
+
+    def _extract_from_df(self, df):
+        """
+        Extract multiplex rows from a dataframe
+
+        """
+
+        edf = df.copy()
+        edf.index = df["primer_name"]
+
+        # Extract associated dataframes
+        multiplex_dfs = [edf.loc[m.get_primer_names()] for m in self.top_multiplexes]
+
+        return multiplex_dfs
+
+    def set_top_multiplexes(self, top_N=3):
+        """
+        Create a smaller list of the `top_N` multiplexes
+
+        """
+
+        # Limit to `top_N` multiplexes
+        self.top_N = top_N
+        self.top_multiplexes = self.uniq_multiplexes[:top_N]
+
+    def get_union_dataframe(self, output_path=None):
+        """
+        Write a union dataframe giving information about union of
+        primers across all multiplexes
+
+        """
+
+        # Extract multiplex dataframes
+        dfs = self._extract_from_df(df=self.primer_df)
+
+        # Combine
+        union_df = (
+            pd.concat(dfs)
+            .reset_index(drop=True)
+            .fillna(False)
+            .drop_duplicates("primer_name")
+            .sort_values("primer_name")
+        )
+
+        # Add inclusion column
+        for ix in range(self.top_N):
+
+            in_multiplex = [
+                p in self.top_multiplexes[ix].primer_pairs
+                for p in union_df["pair_name"]
+            ]
+
+            union_df.insert(ix + 4, f"in_multiplex{ix:02d}", in_multiplex)
+
+        # Store
+        self.union_df = union_df
+
+        # Optionally write
+        if output_path is not None:
+            self.union_df.to_csv(output_path, index=False)
+
+    def get_order_dataframe(self, output_path=None):
+        """Write dataframe for ordering"""
+        assert self.union_df is not None, "Must run `.get_union_dataframe()` first."
+
+        # Create smaller datafram with columns for IDT order of primers
+        order_df = self.union_df[["primer_name", "seq"]]
+        order_df.insert(2, "concentration", "25nm")
+        order_df.insert(3, "purification", "STD")
+
+        # Store
+        self.order_df = order_df
+
+        if output_path is not None:
+            self.order_df.to_csv(output_path, index=False)
