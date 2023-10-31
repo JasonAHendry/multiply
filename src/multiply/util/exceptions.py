0dbbcffe5bd1f4a0992d8df19449c765a5469306
JasonAHendry <jason.ahendry@gmail.com>
2023-10-31 21:55:00 +0100

    Switch download from RefSeq to use 'assembly_summary.txt' files to find FTP.

--- src/multiply/util/exceptions.py
+++ src/multiply/util/exceptions.py
@@ -1,31 +1,46 @@
 class DesignFileError(Exception):
-    """ Error in format or contents of the design file """
+    """Error in format or contents of the design file"""
+
     pass
+
 
 class NoTargetsFoundError(Exception):
-    """ Error finding targets, usually related to GFF """
+    """Error finding targets, usually related to GFF"""
+
     pass
+
 
 class TargetSizeError(Exception):
-    """ Error in size of a target, usually relative to specified parameters """
+    """Error in size of a target, usually relative to specified parameters"""
+
     pass
+
 
 class TargetPositionError(Exception):
-    """ Error in the position of targets, i.e. some overlap """
+    """Error in the position of targets, i.e. some overlap"""
+
     pass
+
 
 class NoPrimerNameException(Exception):
-    """ Raised when the `primer_name` column is missing """
+    """Raised when the `primer_name` column is missing"""
+
     pass
+
 
 class NoPrimersFoundException(Exception):
-    """ Raised  when no primers are found for a target """
+    """Raised  when no primers are found for a target"""
+
     pass
 
-class GenomeSourceNotFound(Exception):
-    """ Raised when a genome source is invalid """
+
+class GenomeCollectionError(Exception):
+    """Error in format or contents of the genome collection file"""
+
     pass
+
 
 class BEDFormattingError(Exception):
-    """ Raised when an input BED file is formatted incorrectly """
+    """Raised when an input BED file is formatted incorrectly"""
+
     pass
